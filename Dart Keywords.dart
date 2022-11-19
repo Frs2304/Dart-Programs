03c3fac7810780efdc03c4edb8214f13286abbdf
Tushar Mishra <you@example.com>
2022-11-19 23:43:41 +0530

    Dart Keywords

---
+++ Dart Keywords.dart
@@ -0,0 +1,18 @@
+void main() {
+// var keyword
+  //  var keyword is used to decalare numbers
+  var a1 = 5;
+  var a2 = 6;
+  print(a1 + a2);
+  // prints output i.e. 11
+
+// int keyword
+  // int keyword stores integer
+  int a = 2;
+  int b = 3;
+  print(a + b);
+
+// dynamic keyword
+  // dynamic keyword automatically detects the input type
+  dynamic name = 8;
+}

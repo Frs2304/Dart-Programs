afc58da6f131aa772dd24a5931608a4b346bc73a
Tushar Mishra <you@example.com>
2022-11-19 23:41:58 +0530

    Dart Keywords

---
+++ tempCodeRunnerFile.dart
@@ -0,0 +1,12 @@
+void main(){
+  //  var keyword is used to decalare a number
+  var a1 = 5;
+  var a2 = 6;
+  print (a1+a2);
+  // prints output i.e. 11
+
+  int a = 2;
+  int b = 3;
+  print(a+b);
+  
+}

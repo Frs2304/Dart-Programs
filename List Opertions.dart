ec13e9cface939815214797d6b356f656f6546ff
Tushar Mishra <tusharmishra2304@gmail.com>
2022-11-22 23:48:08 +0530

    Add files via upload
    List operation in Dart

---
+++ List Opertions.dart
@@ -0,0 +1,9 @@
+void main() {
+  List first = [1, 2, 3, 4, 5];
+// Remove operation
+  first.remove(2);
+  print(first);
+// Empty Funtion to check whether the list is empty or not
+// returns true or false
+  print(first.isEmpty);
+}

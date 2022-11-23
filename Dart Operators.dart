04ccc550c5b8f8846f4541b877ec1321dae3d29e
Tushar Mishra <tusharmishra2304@gmail.com>
2022-11-23 23:58:33 +0530

    Operators in Dart

---
+++ Dart Operators.dart
@@ -0,0 +1,15 @@
+void main() {
+  print("Example of Assignment operators");
+  var n1 = 10;
+  var n2 = 5;
+  // addition
+  print("n1+n2 = ${n1 + n2}");
+  // substraction
+  print("n1-n2 = ${n1 - n2}");
+  // multiplication
+  print("n1*n2 = ${n1 * n2}");
+  // division
+  print("n1/=n2 = ${n1 / n2}");
+  // modulo
+  print("n1%n2 = ${n1 % n2}");
+}

33370e9277b665328cd6dd5ab688445b8ab50a35
Tushar Mishra <tusharmishra2304@gmail.com>
2022-11-28 23:17:16 +0530

    Catch exception in Dart

---
+++ Catch Exception.dart
@@ -0,0 +1,13 @@
+void main() {
+  int x = 12;
+  int y = 0;
+  int res;
+
+  try {
+    res = x ~/ y;
+  }
+// It returns the built-in exception related to the occurring exception
+  catch (E) {
+    print(E);
+  }
+}

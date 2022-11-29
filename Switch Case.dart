9697c61deea7b85cb79c61c27fb937159df24703
Tushar Mishra <tusharmishra2304@gmail.com>
2022-11-29 21:21:19 +0530

    Switch Case Statements in Dart

---
+++ Switch Case.dart
@@ -0,0 +1,20 @@
+void main() {
+  int n = 3;
+  switch (n) {
+    case 1:
+      print("Value is 1");
+      break;
+    case 2:
+      print("Value is 2");
+      break;
+    case 3:
+      print("Value is 3");
+      break;
+    case 4:
+      print("Value is 4");
+      break;
+    default:
+      print("Out of range");
+      break;
+  }
+}

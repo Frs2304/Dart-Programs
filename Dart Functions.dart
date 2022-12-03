4b86eaf3d48b7cc2fbf730a1ca765aa0ff04f72e
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-03 23:56:25 +0530

    Update Dart Functions.dart

--- Dart Functions.dart
+++ Dart Functions.dart
@@ -1,7 +1,8 @@
+// creating a function
 manager(int qnty, String pcs, String item) {
   print('manager');
 }
-
 void main() {
+  // calling the user defined function in main function
   manager(5, "pcs", "pants");
 }

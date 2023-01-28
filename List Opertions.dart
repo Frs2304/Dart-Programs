2e4a8e5fa93fbab91df32cc6d256404ccd5aa388
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-28 23:30:59 +0530

    Update List Opertions.dart

--- List Opertions.dart
+++ List Opertions.dart
@@ -1,9 +1,9 @@
 void main() {
   List first = [1, 2, 3, 4, 5];
-// Remove operation
+// Remove operation removes the elemnt given in the input
   first.remove(2);
   print(first);
 // Empty Funtion to check whether the list is empty or not
 // returns true if list is empty else returns false
-  print(first.isEmpty);
+  print(first.isEmpty); 
 }
